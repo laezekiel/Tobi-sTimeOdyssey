47aee8019c398da1ebe4ff7026634e6cfc72866c
laezekiel <louisboure@hotmail.fr>
2023-04-06 11:15:03 +0200

    SQL test and everything else

--- Tobi-sTimeOdysseyV1/Scripts/Tools/PlayTest.cs
+++ Tobi-sTimeOdysseyV1/Scripts/Tools/PlayTest.cs
@@ -8,14 +8,14 @@ namespace Com.IronicEntertainment.TobisTimeOdyssey.Tools
 {
     public static class PlayTest
     {
-        public static bool StartAtBegining = true;
+        //public static bool StartAtBegining = true;
 
-        public static bool IgnoreAllCutScenes = true;
-        public static bool IgnoreBeginingCutScenes = false;
-        public static bool IgnoreEndCutScenes = false;
+        //public static bool IgnoreAllCutScenes = true;
+        //public static bool IgnoreBeginingCutScenes = false;
+        //public static bool IgnoreEndCutScenes = false;
 
-        public static bool ResetAllCutScenes = true;
-        public static bool ResetBeginingCutScenes = false;
-        public static bool ResetEndCutScenes = false;
+        //public static bool ResetAllCutScenes = true;
+        //public static bool ResetBeginingCutScenes = false;
+        //public static bool ResetEndCutScenes = false;
     }
 }
